15e99fa48d1fd98431fee4223427108c7759599b
Tucker <takahirosuzuki.m010@gmail.com>
2017-08-02 20:49:28 +1000

    created pages/properties

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :rooms
   root 'pages#home'
 
+  get 'pages/properties'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
