bdcfe557c9360975c810e4bdaab1978da270ce18
Tucker <takahirosuzuki.m010@gmail.com>
2017-08-15 20:45:13 +1000

    admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,13 @@
 class AdminController < ApplicationController
+  before_action :authenticete_user, only: :index
+
   def index
     @rooms = Room.all
     @users = User.all
+  end
+
+  def authorize_user
+    if current_user.user_attribute != 'admin'
+      redirect_to root_path, notice: 'Access DENIED'  
   end
 end
